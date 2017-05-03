a76fec86837143ab5086516bdfbded0651c69e19
Billy Wong <bwong5@stuy.edu>
2017-05-03 15:23:52 -0400

    rid of header

--- script.py
+++ script.py
@@ -3,24 +3,6 @@ from display import *
 from matrix import *
 from draw import *
 
-
-'''
-push
-`push a copy of the current top of the origins stack onto the origins stack (a full copy, not just a reference to the current top)
-pop
-`removes the top of the origins stack (nothing needs to be done with this data)
-move/rotate/scale
-`create a translation/rotation/scale matrix and multiply the current top by it
-box/sphere/torus
-`add a box/sphere/torus to a temporary polygon matrix, multiply it by the current top and draw it to the screen 
-line
-`add a line to a temporary edge matrix, multiply it by the current top and draw it to the screen
-save
-`save the screen with the provided file name
-display
-`show the image
-'''
-
 ARG_COMMANDS = [ 'line', 'scale', 'move', 'rotate', 'save', 'circle', 'bezier', 'hermite', 'box', 'sphere', 'torus' ]
 
 def run(filename):
@@ -45,7 +27,7 @@ def run(filename):
     tmp = []
     step = 0.1
     for command in commands:
-        #print command
+        print command
         if command[0] in ARG_COMMANDS:
             args = command[1:]
         
